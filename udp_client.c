c5ddc2bfc2fbf56c5a4a68e52daed5d7835131f6
Vishal Vunnam <vivu9928@colorado.edu>
2025-10-11 16:19:40 -0600

    why

--- udp_client.c
+++ udp_client.c
@@ -10,9 +10,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <time.h>
+#include <ctype.h>
 
 #define BUFSIZE 1024
-
+#define SWS 4
+#define ARRAY_SIZE 25
+#define ACK_TIMEOUT 10
 /* 
  * error - wrapper for perror
  */
@@ -21,7 +25,152 @@ void error(char *msg) {
     exit(0);
 }
 
-// int send_request()
+typedef struct {
+   int LAR; 
+   int LFS; 
+   int frame_ptr; 
+   int frame_size;  
+   struct sendQ_slot {
+      char msg[BUFSIZE]; 
+      int acked; 
+      time_t send_time; 
+   } sendQ[ARRAY_SIZE];
+} SWS_info; 
+
+void init_SWS(SWS_info *sender_window, int frame_size) { 
+  sender_window -> LAR = -1; 
+  sender_window -> LFS = -1; 
+  sender_window -> frame_ptr = 0; 
+  sender_window -> frame_size = frame_size; 
+  for (int i = 0; i < ARRAY_SIZE; i++) { 
+    sender_window -> sendQ[i].acked = 1; 
+    memset(sender_window->sendQ[i].msg, 0, BUFSIZE);
+  }
+}
+
+void get_handler(char *buf) {
+    if (!buf) return;
+
+    // Make a modifiable copy
+    char line[8192];
+    strncpy(line, buf, sizeof(line));
+    line[sizeof(line) - 1] = '\0';
+
+    // Split by '|'
+    strtok(line, "|");               // skip ACK num
+    char *filename = strtok(NULL, "|");
+    char *filedata = strtok(NULL, "");
+
+    if (!filename || !filedata) {
+        fprintf(stderr, "Invalid format: %s\n", buf);
+        return;
+    }
+
+    // Trim leading/trailing spaces
+    while (isspace((unsigned char)*filename)) filename++;
+    char *end = filename + strlen(filename) - 1;
+    while (end > filename && isspace((unsigned char)*end)) *end-- = '\0';
+
+    while (isspace((unsigned char)*filedata)) filedata++;
+    end = filedata + strlen(filedata) - 1;
+    while (end > filedata && isspace((unsigned char)*end)) *end-- = '\0';
+
+    // Append data to file
+    FILE *fp = fopen(filename, "ab");  // "ab" = append binary (works for text too)
+    if (!fp) {
+        perror("fopen");
+        return;
+    }
+
+    fprintf(fp, "%s", filedata);
+    fclose(fp);
+
+    printf("Appended data to file: %s\n", filename);
+}
+
+
+void reset_timeout(struct timeval *timeout) { 
+  timeout->tv_sec = 1; 
+  timeout->tv_usec = 0; 
+}
+
+int handle_ACK(char* buf, SWS_info *sender_window) { 
+    int acknum = -1;
+
+    // 1. Try to extract the ack number (up to '|')
+    if (sscanf(buf, "%d", &acknum) != 1) {
+        fprintf(stderr, "Malformed ACK: %s\n", buf);
+        return -1;
+    }
+
+    printf("Received ACK for frame: %d\n", acknum);
+    if (acknum < 0 || acknum >= ARRAY_SIZE) { 
+        fprintf(stderr, "ACK number out of range: %d\n", acknum);
+        return -1;
+    }
+    if (acknum == (sender_window->LAR + 1) % ARRAY_SIZE) { 
+        sender_window->sendQ[acknum].acked = 1; 
+        sender_window->LAR = acknum; 
+        printf("Sliding window forward. New LAR: %d\n", sender_window->LAR);
+        return 0;
+    }
+    else { 
+        get_handler(buf); 
+        printf("Received out-of-order ACK: %d (expected %d)\n", acknum, (sender_window->LAR + 1) % ARRAY_SIZE);
+        return -1;
+    }
+    
+
+}
+int handle_timeout(SWS_info *sender_window, int sockfd, struct sockaddr_in server_addr, int serverlen) { 
+  printf("Timeout occured, resending all frames in a periof");
+    int start = sender_window->LAR + 1; 
+
+    for (int i = start; i < start + SWS; i++) { 
+        char *msg = sender_window->sendQ[i % SWS].msg;
+
+        if (msg == NULL || msg[0] == '\0') {
+            continue;  // skip empty entries
+        }
+        sender_window->sendQ[i % SWS].send_time = time(NULL); 
+        int n = sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&server_addr, serverlen);
+        if (n < 0) { 
+            error("ERROR in sendto"); 
+            return -1; 
+        }
+    }
+
+    return 0; 
+}
+
+int send_frame(char *s_msg, SWS_info *sender_window, int sockfd, struct sockaddr_in server_addr, int serverlen) { 
+    // compute next sequence number
+    int seq_num = (sender_window->LFS + 1) % ARRAY_SIZE; 
+    // construct message buffer
+    char msg_with_seq[BUFSIZE];
+    snprintf(msg_with_seq, sizeof(msg_with_seq), "%d | %s ", seq_num,  s_msg);
+
+    printf("preparing to send: %s\n", msg_with_seq);
+    strcpy(sender_window->sendQ[sender_window->frame_ptr].msg, msg_with_seq);
+    sender_window->sendQ[sender_window->frame_ptr].acked = 0; 
+    sender_window->sendQ[sender_window->frame_ptr].send_time = time(NULL);
+
+
+    if (sender_window->LFS - sender_window->LAR < SWS) { 
+        sender_window->LFS++; 
+
+        int n = sendto(sockfd, msg_with_seq, strlen(msg_with_seq), 0, 
+                      (struct sockaddr *)&server_addr, serverlen);
+        if (n < 0) { 
+            perror("ERROR in sendto");
+            return -1;
+        }
+
+        sender_window->frame_ptr = (sender_window->frame_ptr + 1) % SWS; 
+        return n; 
+    }
+    return 0;
+}
 
 
 int main(int argc, char **argv) {
@@ -30,6 +179,7 @@ int main(int argc, char **argv) {
     struct sockaddr_in serveraddr;
     struct hostent *server;
     char *hostname;
+    SWS_info sender_window; 
     char buf[BUFSIZE];
 
     /* check command line arguments */
@@ -59,21 +209,75 @@ int main(int argc, char **argv) {
 	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
 
-    /* get a message from the user */
-    bzero(buf, BUFSIZE);
-    printf("Please enter msg: ");
-    fgets(buf, BUFSIZE, stdin);
+    // Initialize Sender Window
+    init_SWS(&sender_window, BUFSIZE); 
 
-    /* send the message to the server */
-    serverlen = sizeof(serveraddr);
-    n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-    
-    /* print the server's reply */
-    n = recvfrom(sockfd, buf, strlen(buf), 0, &serveraddr, &serverlen);
-    if (n < 0) 
-      error("ERROR in recvfrom");
-    printf("Echo from server: %s", buf);
-    return 0;
+
+    /* get a message from the user */
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(sockfd, &readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    int maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
+
+    struct timeval tv; 
+    printf("Please enter msg: \n");
+    fflush(stdout);
+
+    while (1){ 
+      printf("Restarting loop \n"); 
+      printf("LAR: %d, LFS: %d, frame_ptr: %d \n", sender_window.LAR, sender_window.LFS, sender_window.frame_ptr);
+      printf("Window: \n");
+      for (int i = 0; i < SWS; i++) { 
+        printf("Frame %d: %s, acked: %d \n", i, sender_window.sendQ[i].msg, sender_window.sendQ[i].acked); 
+      }
+
+      if (sender_window.LFS > sender_window.LAR) {
+          // There are outstanding unacked frames
+          int oldest_unacked = (sender_window.LAR + 1) % ARRAY_SIZE;
+          if (sender_window.sendQ[oldest_unacked].send_time != 0 && 
+              difftime(time(NULL), sender_window.sendQ[oldest_unacked].send_time) >= ACK_TIMEOUT) {
+              printf("ACK timeout for frame %d, resending...\n", oldest_unacked);
+              handle_timeout(&sender_window, sockfd, serveraddr, serverlen); 
+          }
+      }
+
+      FD_ZERO(&readfds);
+      FD_SET(sockfd, &readfds);
+      FD_SET(STDIN_FILENO, &readfds);
+      reset_timeout(&tv);
+      int rv = select(maxfd+1, &readfds, NULL, NULL, &tv);
+      if (rv > 0) {
+          if (FD_ISSET(sockfd, &readfds)){
+              int n = recvfrom(sockfd, buf, BUFSIZE, 0, &serveraddr, &serverlen);
+              if (n < 0) error("recvfrom");
+              buf[n] = '\0';  // <-- Important
+              printf("------------------------ \n");
+              printf("ECHO FROM SERVER: %s\n", buf); 
+              printf("------------------------ \n"); 
+
+              handle_ACK(buf, &sender_window); 
+              memset(buf,0, BUFSIZE); 
+          }
+          if (FD_ISSET(STDIN_FILENO, &readfds)) {
+              if (fgets(buf, BUFSIZE, stdin) != NULL) {
+                  serverlen = sizeof(serveraddr);
+                  buf[strcspn(buf, "\n")] = '\0';
+                  send_frame(buf, &sender_window, sockfd, serveraddr, serverlen);
+              }
+              printf("Please enter msg: \n ");
+              fflush(stdout);
+              memset(buf, 0, BUFSIZE);
+          }
+      }  // ← Close the "if (rv > 0)" block HERE
+      else if (rv == 0) {  // ← NOW this is at the right level
+          if (sender_window.LFS > sender_window.LAR) {
+              handle_timeout(&sender_window, sockfd, serveraddr, serverlen);
+          }
+      }
+      else {  // rv < 0, actual error
+          perror("select"); 
+      }
+    }
 }
--- udp_server.c
+++ udp_server.c
@@ -12,12 +12,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <dirent.h> // for handle_ls
 
 #define BUFSIZE 1024
-
-/*
- * error - wrapper for perror
- */
+#define RWS 4
+#define ARRAY_SIZE 25
 
 typedef struct {
     int sockfd;
@@ -25,168 +24,346 @@ typedef struct {
     int clientlen;
 } client_res_info;
 
-void error(char *msg) {
-  perror(msg);
-  exit(1);
+typedef struct {
+   int LFR; 
+   int LAF; 
+   int frame_ptr; 
+   int frame_size;  
+   struct recQ_slot {
+    int received; 
+    char msg[BUFSIZE];
+   } recQ[ARRAY_SIZE];
+} RWS_info; 
+
+/* Function declarations */
+void handle_ls(client_res_info response_info, char *buf, int ack_num);
+void handle_exit(client_res_info response_info, char *buf, int ack_num);
+void handle_get(client_res_info response_info, char *buf, int ack_num);
+void handle_put(client_res_info response_info, char *buf, int ack_num);
+void handle_delete(client_res_info response_info, char *buf, int ack_num);
+void error(const char *msg);
+int sender(client_res_info response_info, char *response, char *return_code, int acknum);
+int input_transfer(client_res_info response_info, char *buf, int acknum);
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
 }
 
-int sender(client_res_info response_info, char *response, char *return_code) { 
+void init_RWS(RWS_info *receiver_window, int frame_size) { 
+    receiver_window->LFR = -1; 
+    receiver_window->LAF = -1;
+    receiver_window->frame_ptr = 0; 
+    receiver_window->frame_size = frame_size; 
+    for (int i = 0; i < ARRAY_SIZE; i++) { 
+        receiver_window->recQ[i].received = 0; 
+        memset(receiver_window->recQ[i].msg, 0, BUFSIZE);
+    } 
+}
+int window_handler(int ack_num, RWS_info *receiver_window) { 
+    if(ack_num == (receiver_window->LFR + 1) % RWS) { 
+        receiver_window->LFR = (receiver_window->LFR + 1) % ARRAY_SIZE; 
+        return 0; 
+    }
+    else { 
+
+        printf("Out of order ACK received: %d\n, expected %d", ack_num, (receiver_window->LFR + 1) % ARRAY_SIZE);
+        return -1; 
+    }
+}
+
+int sender(client_res_info response_info, char *response, char *return_code, int acknum) { 
+    char send_buf[BUFSIZE];
+
+
+    // Format: "<acknum> | <response>"
+    if (acknum < 0) snprintf(send_buf, sizeof(send_buf), "NA | %s", response); 
+    else snprintf(send_buf, sizeof(send_buf), "%d | %s", acknum, response);
+
+    printf("SENDING: %s\n", send_buf);
+    printf("TO: %s\n", inet_ntoa(response_info.clientaddr.sin_addr));
+    printf("Length: %d\n", (int)strlen(send_buf)); 
+
     int n = sendto(
         response_info.sockfd, 
-        response, 
-        strlen(response), 
+        send_buf, 
+        strlen(send_buf), 
         0, 
         (struct sockaddr *) &response_info.clientaddr, 
         response_info.clientlen
     );
 
     if (n < 0) {
-        error("Failed to send response");
+        perror("Failed to send response");
         return -1;
     }
 
-    // Optionally handle return_code if needed later
-    // (e.g., strcpy(return_code, "200 OK");)
-
+    (void)return_code; // suppress unused warning
     return n;
 }
 
-int input_transfer(client_res_info response_info, char *buf) {
+int input_transfer(client_res_info response_info, char *buf, int acknum) {
     char command[25];
+    // 2. Extract base command (first token)
+    sscanf(buf, "%24s", command);
 
-    buf[strcspn(buf, "\n")] = '\0';
-
-    if (strcmp(buf, "ls") == 0) {
-        handle_ls(response_info, buf);
+    if (strcmp(command, "ls") == 0) {
+        handle_ls(response_info, buf, acknum);
     } 
-    else if (strcmp(buf, "exit") == 0) {
-        handle_exit(response_info, buf);
+    else if (strcmp(command, "exit") == 0) {
+        handle_exit(response_info, buf, acknum);
+    } 
+    else if (strcmp(command, "get") == 0) {
+        handle_get(response_info, buf, acknum);
+    } 
+    else if (strcmp(command, "put") == 0) {
+        handle_put(response_info, buf, acknum);
+    } 
+    else if (strcmp(command, "delete") == 0) {
+        handle_delete(response_info, buf, acknum);
     } 
     else {
-        sscanf(buf, "%24s", command);
-
-        if (strcmp(command, "get") == 0) {
-            handle_get(response_info, buf);
-        } 
-        else if (strcmp(command, "put") == 0) {
-            handle_put(response_info, buf);
-        } 
-        else if (strcmp(command, "delete") == 0) {
-            handle_delete(response_info, buf);
-        } 
-        else {
-            error("Request type was invalid");
-        }
+        fprintf(stderr, "Invalid request: %s\n", buf);
+        sender(response_info, "Invalid command", NULL, acknum); 
     }
 
-    return 0;
+    return acknum; // useful for caller
 }
 
-void handle_get(client_res_info response_info, char *buf) {
-    // Example: parse "get filename"
-    printf("filename", filename);
-    if (filename != NULL)
-        filename += 4; // skip "get "
-    else
+
+void handle_get(client_res_info response_info, char *buf, int ack_num) {
+    if (buf == NULL) return;
+    buf += 4; // skip "get "
+    buf[strcspn(buf, "\r\n ")] = '\0'; 
+    printf("Requested file: %s\n", buf);
+    FILE *file_ptr = fopen(buf, "rb");
+    if (!file_ptr) {
+        printf("FILE WAS NOT FOUND"); 
+        sender(response_info, "File not found", NULL, ack_num); 
+        perror("File not found");
         return;
+    }
 
-    // TODO: implement file retrieval logic
+    sender(response_info, "Starting file transfer", NULL, ack_num); 
+    fseek(file_ptr, 0, SEEK_END);
+    long file_size = ftell(file_ptr);
+    fseek(file_ptr, 0, SEEK_SET);
+
+    printf("Sending file: %s (%ld bytes)\n", buf, file_size);
+
+    char file_buffer[BUFSIZE];
+    size_t bytes_read;
+    int frame_num = 0;
+
+    while ((bytes_read = fread(file_buffer, 1, BUFSIZE - 50, file_ptr)) > 0) {
+        // Frame the packet: "<ack_num> | <data>"
+        char packet[BUFSIZE];
+        memset(packet, 0, sizeof(packet));
+        
+        int len = snprintf(packet, sizeof(packet), "%s | ", buf);
+
+        // Make sure we don’t overflow
+        memcpy(packet + len, file_buffer, bytes_read);
+        len += bytes_read;
+
+        // Send the framed message
+        int n = sender(response_info, packet, NULL, ack_num); 
+
+        if (n < 0) {
+            perror("sendto failed");
+            break;
+        }
+
+        printf("Sent frame %d (%zu bytes)\n", ack_num + frame_num, bytes_read);
+        frame_num++;
+
+        // Optionally wait for ACK here before sending next frame (if reliable)
+    }
+
+    fclose(file_ptr);
+
+    // Optionally send end-of-file marker
+    char end_msg[64];
+    snprintf(end_msg, sizeof(end_msg), "%d | END", ack_num);
+    sender(response_info, end_msg, NULL, ack_num); 
+
+    printf("File transfer complete.\n");
 }
 
-void handle_put(client_res_info response_info, char *buf) {
+
+void handle_put(client_res_info response_info, char *buf, int ack_num) {
+    if (buf != NULL) buf += 4; 
+    else return; 
+
+
+
+
+
+
+    (void)response_info;
+    (void)buf;
     // TODO: implement file upload logic
+
 }
 
-void handle_delete(client_res_info response_info, char *buf) {
-    // TODO: implement delete logic
+void handle_delete(client_res_info response_info, char *buf, int ack_num) {
+    (void)response_info;
+    if (buf != NULL) buf += 7;
+    else return;
+
+    int status = remove(buf);
+
+    if (status == 0) {
+        printf("File '%s' deleted successfully.\n", buf);
+    } else {
+        printf("Error deleting file '%s'.\n", buf);
+        perror("Error details"); 
+    }
 }
 
-void handle_ls(client_res_info response_info, char *buf) {
-    // TODO: implement directory listing logic
+void handle_ls(client_res_info response_info, char *buf, int ack_num) {
+    (void)buf;
+    char *dir_buff = NULL; 
+    dir_buff = (char *)malloc(BUFSIZE);
+    if (dir_buff == NULL) {
+        perror("malloc failed");
+        return;
+    }
+
+    // Get the current working directory
+    if (getcwd(dir_buff, BUFSIZE) != NULL) {
+        printf("Current working directory: %s\n", dir_buff);
+    } else {
+        perror("getcwd failed");
+    }
+
+    DIR *dir = opendir(".");
+    if (!dir) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    char response[BUFSIZE] = "";
+    while ((entry = readdir(dir)) != NULL) {
+        strcat(response, entry->d_name);
+        strcat(response, ",");
+    }
+    closedir(dir);
+    free(dir_buff); 
+    sender(response_info, response, NULL, ack_num);
+    return; 
 }
 
-void handle_exit(client_res_info response_info, char *buf) {
-    // TODO: implement client disconnect logic
+void handle_exit(client_res_info response_info, char *buf, int ack_num) {
+    (void)response_info;
+    (void)buf;
+    printf("Client requested exit.\n");
+    sender(response_info, "Goodbye!\n", NULL, ack_num); 
+    exit(0); 
+    // In UDP, we don't have a persistent connection to close.
 }
 
 int main(int argc, char **argv) {
-  setvbuf(stdout, NULL, _IONBF, 0);
-  int sockfd; /* socket */
-  int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
-  struct sockaddr_in serveraddr; /* server's addr */
-  struct sockaddr_in clientaddr; /* client addr */
-  struct hostent *hostp; /* client host info */
-  char buf[BUFSIZE]; /* message buf */
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
+    setvbuf(stdout, NULL, _IONBF, 0);
+    int sockfd;
+    int portno;
+    int clientlen;
+    struct sockaddr_in serveraddr;
+    struct sockaddr_in clientaddr;
+    struct hostent *hostp;
+    char buf[BUFSIZE];
+    char *hostaddrp;
+    int optval;
+    int n;
 
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    portno = atoi(argv[1]);
 
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
 
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) 
-    error("ERROR opening socket");
+    optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
+               (const void *)&optval , sizeof(int));
 
-  optval = 1;
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-	     (const void *)&optval , sizeof(int));
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
 
-  bzero((char *) &serveraddr, sizeof(serveraddr));
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serveraddr.sin_port = htons((unsigned short)portno);
-
-  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-	   sizeof(serveraddr)) < 0) 
-    error("ERROR on binding");
-
-  printf("Server listening on port %d", portno);
+    if (bind(sockfd, (struct sockaddr *) &serveraddr, 
+             sizeof(serveraddr)) < 0) 
+        error("ERROR on binding");
 
 
-  clientlen = sizeof(clientaddr);
-  while (1) {
-    /*
-     * recvfrom: receive a UDP datagram from a client
-     */
-    bzero(buf, BUFSIZE);
-    n = recvfrom(sockfd, buf, BUFSIZE, 0,
-		 (struct sockaddr *) &clientaddr, &clientlen);
-    if (n < 0)
-      error("ERROR in recvfrom");
+    RWS_info receiver_window; 
+    init_RWS(&receiver_window, BUFSIZE); 
 
-    /* 
-     * gethostbyaddr: determine who sent the datagram
-     */
-    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
-			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-    if (hostp == NULL)
-      error("ERROR on gethostbyaddr");
-    hostaddrp = inet_ntoa(clientaddr.sin_addr);
-    if (hostaddrp == NULL)
-      error("ERROR on inet_ntoa\n");
-    printf("server received datagram from %s (%s)\n", 
-	   hostp->h_name, hostaddrp);
-    printf("server received %d/%d bytes: %s\n", strlen(buf), n, buf);
-    
-    /* 
-     * sendto: echo the input back to the client 
-     */
-    client_res_info client_info;  // ✅ Declare variable
 
-    client_info.sockfd = sockfd;
-    client_info.clientaddr = clientaddr;
-    client_info.clientlen = clientlen;
+    printf("Server listening on port %d\n", portno);
 
-    input_transfer(client_info, buf); 
-    n = sendto(sockfd, buf, strlen(buf), 0, 
-	       (struct sockaddr *) &clientaddr, clientlen);
-    if (n < 0) 
-      error("ERROR in sendto");
-  }
+    clientlen = sizeof(clientaddr);
+    while (1) {
+        bzero(buf, BUFSIZE);
+        n = recvfrom(sockfd, buf, BUFSIZE, 0,
+                     (struct sockaddr *) &clientaddr, (socklen_t *) &clientlen);
+        if (n < 0)
+            error("ERROR in recvfrom");
+
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+                              sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL)
+            error("ERROR on gethostbyaddr");
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL)
+            error("ERROR on inet_ntoa");
+        printf("server received datagram from %s (%s)\n", 
+               hostp->h_name, hostaddrp);
+        printf("server received %d/%d bytes: %s\n", (int)strlen(buf), n, buf);
+        
+        client_res_info client_info;
+        client_info.sockfd = sockfd;
+        client_info.clientaddr = clientaddr;
+        client_info.clientlen = clientlen;
+
+        if (n > 0) {
+            int acknum = -1;
+            char *cmd_start = strstr(buf, "|");
+
+            if (cmd_start == NULL) {
+                fprintf(stderr, "Malformed message (no '|'): %s\n", buf);
+                return -1;
+            }
+            *cmd_start = '\0';
+
+            // Extract acknum
+            if (sscanf(buf, "%d", &acknum) != 1) {
+                fprintf(stderr, "Malformed ACK number: %s\n", buf);
+                return -1;
+            }
+
+            // Move to command part (skip '|' and spaces)
+            cmd_start++;
+            while (*cmd_start == ' ') cmd_start++;
+            cmd_start[strcspn(cmd_start, "\n")] = '\0'; // remove newline
+
+            printf("ACKNUM: %d\n", acknum);
+            printf("Command buffer: %s\n", cmd_start);
+
+            // Pass *only* the command part to input_transfer
+            if (window_handler(acknum, &receiver_window) ==0) input_transfer(client_info, cmd_start, acknum);
+            else printf("Ignoring out-of-order ACK: %d\n", acknum);
+        }
+        else {
+            fprintf(stderr, "Received NULL buffer.\n");
+        }
+
+        if (n < 0) 
+            error("ERROR in sendto");
+    }
 }
